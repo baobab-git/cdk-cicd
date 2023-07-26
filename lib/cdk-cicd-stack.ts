74c1ff2de380b2df904463dff10b6c3392204bd2
baobab <baobab@baba.com>
2023-07-26 17:58:56 +0000

    Add lambda stack and test stage

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Awesome Pipeline', {
+    const pipeline = new CodePipeline(this, 'Awesome Pipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('baobab-git/cdk-cicd', 'main'),
@@ -15,7 +16,11 @@ export class CdkCicdStack extends cdk.Stack {
           'npx cdk synth'
         ],
       })
-    })
+    });
+
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage', {
+      stageName: 'test'
+    }))
 
   }
 }
